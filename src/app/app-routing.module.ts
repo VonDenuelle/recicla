beaa8fd3bd044b8d2dbc765cfa37df36be474a18
Von Denuelle <78338603+VonDenuelle@users.noreply.github.com>
2021-11-03 13:24:15 +0800

    ep8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { PickupCallPageModule } from './pages/pickup-call/pickup-call.module';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'loader',
     pathMatch: 'full'
   },
   {
@@ -20,18 +20,7 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   },
-  // {
-  //   path: 'home',
-  //   loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  // },
-  // {
-  //   path: 'pickup-call',
-  //   loadChildren: () => import('./pages/pickup-call/pickup-call.module').then( m => m.PickupCallPageModule)
-  // },
-  // {
-  //   path: 'pickup-calls',
-  //   loadChildren: () => import('./pages/pickup-calls/pickup-calls.module').then( m => m.PickupCallsPageModule)
-  // },
+
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
--- src/app/pages/home/home.page.spec.ts
+++ src/app/pages/home/home.page.spec.ts
@@ -1,24 +1,36 @@
+import { Route } from '@angular/compiler/src/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { HomePage } from './home.page';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let router: Router
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(),AppRoutingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
+    router=TestBed.get(Router);
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should go to pick up calls/pending', () => {
+    spyOn(router, 'navigate')
+    component.goToPickupCalls();
+    expect(router.navigate).toHaveBeenCalledWith(['pickup-calls']);
+  });
+  it('should go to pick up call', () => {
+    spyOn(router, 'navigate')
+    component.newPickupCall();
+    expect(router.navigate).toHaveBeenCalledWith(['pickup-call']);
   });
 });
